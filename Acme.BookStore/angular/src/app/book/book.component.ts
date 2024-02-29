3078cac8b3db42918480c65c714dc99587c07a78
Nguyễn Minh Tuấn <86965221+MinhTuanCoder@users.noreply.github.com>
2024-02-29 16:44:13 +0700

    Part 3: Create Book
    Sử dụng Component và Modual để tạo form điền

--- Acme.BookStore/angular/src/app/book/book.component.ts
+++ Acme.BookStore/angular/src/app/book/book.component.ts
@@ -1,23 +1,62 @@
 import { ListService, PagedResultDto } from '@abp/ng.core';
 import { Component, OnInit } from '@angular/core';
-import { BookService, BookDto } from '@proxy/books';
-
+import { NgbDateNativeAdapter, NgbDateAdapter } from '@ng-bootstrap/ng-bootstrap';
+import { BookService, BookDto, bookTypeOptions } from '@proxy/books'; // add bookTypeOptions
+import { FormGroup, FormBuilder, Validators } from '@angular/forms'; // add this
 @Component({
   selector: 'app-book',
   templateUrl: './book.component.html',
   styleUrls: ['./book.component.scss'],
-  providers: [ListService],
+  providers: [ListService, { provide: NgbDateAdapter, useClass: NgbDateNativeAdapter } // add this line
+  ],
 })
 export class BookComponent implements OnInit {
   book = { items: [], totalCount: 0 } as PagedResultDto<BookDto>;
+  form: FormGroup; // add this line
 
-  constructor(public readonly list: ListService, private bookService: BookService) {}
+  // add bookTypes as a list of BookType enum members
+  bookTypes = bookTypeOptions;
+
+  isModalOpen = false; // add this line
+  
+  constructor(
+    public readonly list: ListService,
+    private bookService: BookService,
+    private fb: FormBuilder // inject FormBuilder
+  ) {}
 
   ngOnInit() {
     const bookStreamCreator = (query) => this.bookService.getList(query);
 
     this.list.hookToQuery(bookStreamCreator).subscribe((response) => {
       this.book = response;
+    });
+  }
+  createBook() {
+    this.buildForm(); // add this line
+    this.isModalOpen = true;
+  }
+  
+  // add buildForm method
+  buildForm() {
+    this.form = this.fb.group({
+      name: ['', Validators.required],
+      type: [null, Validators.required],
+      publishDate: [null, Validators.required],
+      price: [null, Validators.required],
+    });
+  }
+
+  // add save method
+  save() {
+    if (this.form.invalid) {
+      return;
+    }
+
+    this.bookService.create(this.form.value).subscribe(() => {
+      this.isModalOpen = false;
+      this.form.reset();
+      this.list.get();
     });
   }
 }
