7b11a7ef14a238f51b25827a12c099401d23cc3e
Nguyễn Minh Tuấn <86965221+MinhTuanCoder@users.noreply.github.com>
2024-02-29 17:01:55 +0700

    Part 6: Delete Book

--- Acme.BookStore/angular/src/app/book/book.component.ts
+++ Acme.BookStore/angular/src/app/book/book.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { NgbDateNativeAdapter, NgbDateAdapter } from '@ng-bootstrap/ng-bootstrap';
 import { BookService, BookDto, bookTypeOptions } from '@proxy/books'; // add bookTypeOptions
 import { FormGroup, FormBuilder, Validators } from '@angular/forms'; // add this
+import { ConfirmationService, Confirmation } from '@abp/ng.theme.shared'; 
 @Component({
   selector: 'app-book',
   templateUrl: './book.component.html',
@@ -20,11 +21,13 @@ export class BookComponent implements OnInit {
 
   isModalOpen = false; // add this line
   
-  constructor(
-    public readonly list: ListService,
-    private bookService: BookService,
-    private fb: FormBuilder // inject FormBuilder
-  ) {}
+  //change the constructor
+constructor(
+  public readonly list: ListService,
+  private bookService: BookService,
+  private fb: FormBuilder,
+  private confirmation: ConfirmationService // inject the ConfirmationService
+) {}
 
   ngOnInit() {
     const bookStreamCreator = (query) => this.bookService.getList(query);
@@ -46,15 +49,27 @@ export class BookComponent implements OnInit {
       this.isModalOpen = true;
     });
   }
+  // Add a delete method
+delete(id: string) {
+  this.confirmation.warn('::AreYouSureToDelete', '::AreYouSure').subscribe((status) => {
+    if (status === Confirmation.Status.confirm) {
+      this.bookService.delete(id).subscribe(() => this.list.get());
+    }
+  });
+}
   // add buildForm method
   buildForm() {
     this.form = this.fb.group({
-      name: ['', Validators.required],
-      type: [null, Validators.required],
-      publishDate: [null, Validators.required],
-      price: [null, Validators.required],
+      name: [this.selectedBook.name || '', Validators.required],
+      type: [this.selectedBook.type || null, Validators.required],
+      publishDate: [
+        this.selectedBook.publishDate ? new Date(this.selectedBook.publishDate) : null,
+        Validators.required,
+      ],
+      price: [this.selectedBook.price || null, Validators.required],
     });
   }
+
 
   // change the save method
   save() {
